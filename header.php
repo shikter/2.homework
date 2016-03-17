a812834554691955da84899009515d8b92e29530
Vadim Kozlov <shikter@greeny.cs.tlu.ee>
2016-03-17 11:23:53 +0200

    added files

---
+++ header.php
@@ -0,0 +1,65 @@
+<?php 
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title of the website</title>
+	
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" >
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" >
+		<!-- jQuery google -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" ></script>
+		
+		
+		
+	<!-- <style>
+		
+   table {
+	border:1px solid;
+    width: 1000px; 		
+	border-collapse: collapse;
+    margin: auto;		 
+   }
+   th { 
+    text-align: left; 		 /* Выравнивание по левому краю */
+    background: #ccc; 		 /* Цвет фона ячеек */
+    padding: 5px; 			 /* Поля вокруг содержимого ячеек */
+    border: 1px solid black; /* Граница вокруг ячеек */
+   }
+   td { 
+    padding: 5px; 			 /* Поля вокруг содержимого ячеек */
+    border: 1px solid black; /* Граница вокруг ячеек */
+	text-align: center; 
+ 
+	</style> -->
+	
+  </head>
+  
+ 
+  <body>
+	
+	
+	
+	
+	
+	
+
+  </body>
+</html>
